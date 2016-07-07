9afefe05c01fc58592537f5b00adee3a7c2a3707
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-07 22:31:29 +0400

    description

--- neerc-semifinal-2015/froggy-ford.cpp
+++ neerc-semifinal-2015/froggy-ford.cpp
@@ -1,9 +1,10 @@
 /**
     Rasul Kerimov (CoderINusE)
+    Froggy Ford
 */
 #include <bits/stdc++.h>
-#define MAXN 1000000
-#define INF 2000000
+#define MAXN 1000
+#define INF 20000000000
 #define _INF -20000000
 #define RANDOM_INIT()	srand(time(NULL))
 #define RANDOM(L, R)	(L + rand() % ((R) - (L) + 1)) // gen a random integer in [L, R]
@@ -11,11 +12,103 @@
 typedef long long ll;
 using namespace std;
 
-vector <vector <int> > vi(15);
-int pos[20];
+typedef pair<ll, ll> pi;
+
+ll w, x, y;
+int n;
+vector <double> minimaxl, minimaxr;
+vector <pi> g;
+
+double dist(const pi &a, const pi &b) {
+    return sqrt((a.first - b.first)*(a.first - b.first) + (a.second - b.second)*(a.second - b.second));
+}
+
+void dijkstra_left() {
+    minimaxl.resize(MAXN + 1, (double)INF);
+    minimaxl[0] = .0;
+    vector<char> u(MAXN + 1);
+    for(int i = 0; i <= n + 1; ++i) {
+        int v = -1;
+        for(int j = 0; j <= n + 1; ++j)
+            if(!u[j] && (v == -1 || minimaxl[j] < minimaxl[v]))
+                v = j;
+
+        if(minimaxl[v] == INF) break;
+        u[v] = true;
+
+        if(v == n + 1) continue;
+
+        for(int j = 1; j <= n + 1; ++j) {
+            if(j == v) continue;
+            double len = .0;
+            if(v == 0) {
+                len = (double)g[j].first;
+                if(j == n + 1) len = (double)w;
+            }
+            else if(j == n + 1) len = (double)(w - g[v].first);
+
+            else len = dist(g[v], g[j]);
+
+            len = max(minimaxl[v], len);
+            if(len < minimaxl[j]) {
+                minimaxl[j] = len;
+            }
+        }
+    }
+}
+
+void dijkstra_right() {
+    minimaxr.resize(MAXN + 1, (double)INF);
+    minimaxr[n + 1] = .0;
+    vector<char> u(MAXN + 1);
+    for(int i = 0; i <= n + 1; ++i) {
+        int v = -1;
+        for(int j = 0; j <= n + 1; ++j)
+            if(!u[j] && (v == -1 || minimaxr[j] < minimaxr[v]))
+                v = j;
+
+        if(minimaxr[v] == INF) break;
+        u[v] = true;
+
+        if(v == 0) continue;
+
+        for(int j = 0; j <= n; ++j) {
+            if(j == v) continue;
+            double len = .0;
+            if(v == n + 1) {
+                len = (double)(w - g[j].first);
+                if(j == 0) len = (double)w;
+            }
+            else if(j == 0) len = (double)(g[v].first);
+
+            else len = dist(g[v], g[j]);
+
+            len = max(minimaxr[v], len);
+            if(len < minimaxr[j]) {
+                minimaxr[j] = len;
+            }
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
+    cin >> n >> w;
+    g.push_back({0, 0});
+    for(int i = 1; i <= n; ++i) {
+        cin >> x >> y;
+        g.push_back({x,y});
+    }
+    dijkstra_left();
+    //dijkstra_right();
 
 
+    for(int i = 0; i <= n + 1; ++i) {
+        cout << minimaxl[i] << endl;
+    }
 
+
+    for(int i = 0; i <= n + 1; ++i) {
+        cout << minimaxr[i] << endl;
+    }
 }
