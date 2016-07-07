0863d1d3d39db9417a3ce0fe57f9d8cb1c2ca413
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-07 23:00:19 +0400

    froggy-ford

--- neerc-semifinal-2015/froggy-ford.cpp
+++ neerc-semifinal-2015/froggy-ford.cpp
@@ -23,6 +23,13 @@ double dist(const pi &a, const pi &b) {
     return sqrt((a.first - b.first)*(a.first - b.first) + (a.second - b.second)*(a.second - b.second));
 }
 
+pair <double, double> find_point(const int &i, const int &j) {
+    if(i == 0 && j == 0) return {(double) w / 2.0, 1.0 };
+    else if(i == 0) return { (double)g[j].first / 2.0 , (double)g[j].second };
+    else if(j == n + 1) return { (double)(w + g[i].first) / 2.0, (double)g[i].second };
+    else return { (double)fabs(g[i].first + g[j].first) / 2.0, (double)fabs(g[i].second + g[j].second) / 2.0 };
+}
+
 void dijkstra_left() {
     minimaxl.resize(MAXN + 1, (double)INF);
     minimaxl[0] = .0;
@@ -102,5 +109,38 @@ int main() {
     dijkstra_left();
     dijkstra_right();
 
+    double ans = INF;
+    pair <double, double> ann;
+    int in, jn;
+
+    for(int i = 0; i <= n + 1; ++i) {
+
+        for(int j = 0; j <= n + 1; ++j) {
+
+            if(i == j) continue;
+
+            double  left = (i == 0 ? .0 : minimaxl[i]),
+                    right = (j == n + 1 ? .0 : minimaxr[j]);
+
+            double mdist = 0.0;
+            if(i == 0 && j == n + 1) mdist = (double)w / 2.0;
+            else if(i == 0) mdist = (double)g[j].first / 2.0;
+            else if(j == n + 1) mdist = (double) (w - g[i].first) / 2.0;
+            else mdist = dist(g[i], g[j]) / 2.0;
+
+            double dist = max(left, max(right, mdist));
+
+            if(dist < ans) {
+
+                ans = dist;
+                ann = find_point(i, j);
+            }
+
+        }
+
+    }
+
+    cout << ans << endl;
+    cout << fixed << setprecision(3) << ann.first << " " << ann.second << endl;
 
 }
