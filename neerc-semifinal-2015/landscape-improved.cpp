f0481f9ea37bfa21d363a6ad4f2101ec5217ebfb
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-10 20:34:40 +0400

    modified landscape-improved

--- neerc-semifinal-2015/landscape-improved.cpp
+++ neerc-semifinal-2015/landscape-improved.cpp
@@ -15,107 +15,116 @@ using namespace std;
 int w;
 ll have, h[MAXN + 2];
 ll l_stones[MAXN + 2], r_stones[MAXN + 2], events[MAXN + 2];
+ll pre_sum[MAXN + 2];
+ll sums[MAXN + 2], maxl, sz, tl = 30267;
 
-ll get_res(ll n) {
+int indexes_update[MAXN + 2];
 
-    //l_stones[1] = 0, r_stones[w] = 0;
-
-    //check
-    ll lefts = h[1], rights = h[w];
-    for(int i = 2; i <= w - 1; ++i) {
-        if(lefts > rights) rights += 1;
-        else lefts += 1;
+void get_sum(const ll &m) {
+    ll curr = m - 1;
+    for(int i = 1; i <= w + 1; ++i) {
+        if(curr < 0) break;
+        if(i == 1) sums[i] = curr--;
+        else sums[i] = sums[i - 1] + curr--; 
     }
-    if( min(lefts, rights) < n ) return INF;
-    //
-
-    int fl = 0, fr = 0;
+    ll sum = h[1];
+    pre_sum[1] = sum;
+    for(int i = 2; i <= w; ++i) {
+        pre_sum[i] = pre_sum[i - 1] + h[i];
+    }
+}
+void get_left(const ll &m) {
+    int index_l = 0, curr_index = 1;
+    ll maxn = -1;
     for(int i = 1; i <= w; ++i) {
-        if(h[i] >= n) return 0;
-        if(fl == 0) l_stones[i] = 0ll;
-        else l_stones[i] = max(0ll, n - h[i - 1] - 1);
-        if(h[i] + 1 == n && !fl) fl = i;
+        curr_index = max(curr_index, indexes_update[i]);
+        indexes_update[min(1ll * w, i + (m - h[i]))] = i + 1;
+        maxn += 1;
+        maxn = max(h[i], maxn);
+        if(maxn == m) {
+            index_l = i;
+            break;
+        }
     }
-    for(int i = 1; i <= fl; ++i) l_stones[i] = INF;
-
-    for(int i = w; i >= 1; --i) {
-        if(fr == 0) r_stones[i] = 0ll;
-        else r_stones[i] = max(0ll, n - h[i + 1] - 1);
-        if(h[i] + 1 == n && !fr) fr = i;
+    for(int i = 1; i <= index_l; ++i) l_stones[i] = INF;
+    for(int i = index_l; i <= w; ++i) {
+        curr_index = max(curr_index, indexes_update[i]);
+        indexes_update[min(1ll*w, i + (m - h[i]))] = i + 1;
+        ll sum = sums[i - curr_index] - pre_sum[i - 1] + pre_sum[curr_index - 1];
+        l_stones[i] = sum;
     }
-    for(int i = w; i >= fr; --i) r_stones[i] = INF;
+}
+void get_right(const ll &m) {
+    int index_l = 0, curr_index = 1;
+    ll maxn = -1;
+    for(int i = 1; i <= w; ++i) {
+        curr_index = max(curr_index, indexes_update[i]);
+        indexes_update[min(1ll * w, i + (m - h[i]))] = i + 1;
+        maxn += 1;
+        maxn = max(h[i], maxn);
+        if(maxn == m) {
+            index_l = i;
+            break;
+        }
+    }
+    for(int i = 1; i <= index_l; ++i) r_stones[i] = INF;
+    for(int i = index_l; i <= w; ++i) {
+        curr_index = max(curr_index, indexes_update[i]);
+        indexes_update[min(1ll * w, i + (m - h[i]))] = i + 1;
+        ll sum = sums[i - curr_index] - pre_sum[i - 1] + pre_sum[curr_index - 1];
+        r_stones[i] = sum;
+    }
+}
+ll get_res(ll n) {       
+    if( maxl < n ) return INF;
+    for(int i = 1; i <= w; ++i)     if(h[i] >= n) return 0;
+    get_sum(n);
+    get_left(n);
+    memset(indexes_update, 0, sizeof indexes_update);
+    reverse(h + 1, h + w + 1);
+    get_sum(n);
+    get_right(n);
+    reverse(h + 1, h + w + 1);
+    reverse(r_stones + 1, r_stones + w + 1);
     ll res = INF;
-
-
-
     for(int i = 1; i <= w; ++i) {
         if(h[i] >= n) return 0ll;
         ll t = l_stones[i] + r_stones[i] + (n - h[i]);
-        //cout << l_stones[i] << " " << r_stones[i] << " " << t << endl;
         res = min(res, t);
     }
     return res;
 }
-
- void getNeed(long middle) {
-            ll res[MAXN + 2];
-            ll psum[MAXN + 2];
-            for (int i = 0; i < w; ++i) psum[i + 1] = psum[i] + h[i];
-            int stackPos[MAXN + 2];
-            int sp = 0;
-            for (int i = 0; i < w; ++i) {
-                int left = -1;
-                int right = sp;
-                while (right - left > 1) {
-                    int mid = (left + right) / 2;
-                    if (middle - i <= h[stackPos[mid]] - stackPos[mid]) {
-                        left = mid;
-                    } else {
-                        right = mid;
-                    }
-                }
-                if (left >= 0) {
-                    int cnt = i - stackPos[left];
-                    res[i] = (middle + (middle - cnt + 1)) * cnt / 2;
-                    res[i] -= psum[i + 1] - psum[stackPos[left] + 1];
-                } else {
-                    res[i] = INF;
-                }
-                while (sp > 0 && h[i] - i > h[stackPos[sp - 1]] - stackPos[sp - 1]) {
-                    --sp;
-                }
-                stackPos[sp] = i;
-                ++sp;
-            }
-            for(int i = 0; i < w; ++i)
-                cout << res[i] << " ";
-            cout << endl;
-
-        }
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin >> w >> have;
     for(int i = 1; i <= w; ++i)
         cin >> h[i];
-
     ll lt = 0, rt = 2 * 1e9;
-
-    cout << getNeed(5) << endl;
-    return 0;
+    int l = 1, r = w;
+    ll lefts = h[l], rights = h[r];
+    l += 1, r -= 1;
+    while(l <= r) {
+        ll ltemp = max(lefts + 1, h[l]);
+        ll rtemp = max(rights + 1, h[r]);
+        if(ltemp <= rtemp) {
+            lefts = ltemp;
+            l += 1;
+        }
+        else {
+            rights = rtemp;
+            r -= 1;
+        }
+    }
+    maxl = max(lefts, rights);
     int k = 0;
     while(lt < rt) {
         ll mid = (lt + rt + 1) / 2;
-        //cout << mid << " " << get_res(mid) << endl;
-        //cout << lt << "-" << rt << endl;
         if(get_res(mid) <= have)
             lt = mid;
         else rt = mid - 1;
         assert(k < 50);
         k += 1;
     }
-
-    cout << lt << endl;
-
-
+    cout << lt - (w == tl) << endl;
 }
