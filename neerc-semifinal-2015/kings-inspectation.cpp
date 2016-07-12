0c05af8362597ce49bb7c37cf000c626aea868ac
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-12 16:29:09 +0400

    kings inspectation added

---
+++ neerc-semifinal-2015/kings-inspectation.cpp
@@ -0,0 +1,17 @@
+/**
+    Rasul Kerimov (CoderINusE)
+    King's Inspectation
+*/
+#include <bits/stdc++.h>
+#define MAXN 100005
+#define INF 2e18
+#define _INF -20000000
+#define RANDOM_INIT()	srand(time(NULL))
+#define RANDOM(L, R)	(L + rand() % ((R) - (L) + 1)) // gen a random integer in [L, R]
+
+typedef long long ll;
+using namespace std;
+vector< vector<int> > g;
+int main() {
+    ios_base::sync_with_stdio(0);
+}
