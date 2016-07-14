eb01d2cfc302433a438bb9ac285e14697e008af9
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-14 23:52:31 +0400

    filter added

---
+++ neerc-semifinal-2014/filter.cpp
@@ -0,0 +1,17 @@
+/**
+    Rasul Kerimov (CoderINusE)
+    Filter
+*/
+#include <bits/stdc++.h>
+#define MAXN 1000005
+#define INF 2e18
+#define _INF -20000000
+#define RANDOM_INIT()   srand(time(NULL))
+#define RANDOM(L, R)    (L + rand() % ((R) - (L) + 1)) // gen a random integer in [L, R]
+
+typedef long long ll;
+using namespace std;
+
+int main() {
+
+}
