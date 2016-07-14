cf60ba9fabac2053fbdbf5c867e6024aee9581e6
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-14 18:00:15 +0400

    jokewithpermutation added

--- neerc-semifinal-2014/jokewithpermutation.cpp
+++ neerc-semifinal-2014/jokewithpermutation.cpp
@@ -12,30 +12,53 @@
 typedef long long ll;
 using namespace std;
 
-int d[55];
+int ds[56], n;
 string s;
-vector<int> ans;
+vector<int> ans, used;
+
 int len(int x) {
-  int len = 0
-  while(x) {
-    len += 1, x /= 10;
-  }
-  return len;
+    int len = 0;
+    while(x) len += 1, x /= 10;
+    return len;
 }
 void init() {
-  int sum = 0;
-  for(int i = 1; i <= 55; ++i) {
-    sum += len(i);
-    d[i] = sum;
-  }
+    int sum = 0;
+    for(int i = 1; i <= 55; ++i) {
+        sum += len(i);
+        ds[i] = sum;
+    }
 }
 
-void f() {
+void f(vector<int>& v, string s, int ind, int num, vector<int>& used) {
 
+    if(ind == s.length()) {
+        if(used[num]) return;
+        if(num) v.push_back(num);
+        for(int i = 1; i < v.size(); ++i) cout << v[i] << " ";
+        exit(0);
+    }
+
+    int temp = num;
+
+    if(!used[num]) {
+        v.push_back(num);
+        used[num] = 1;
+        num = (s[ind] - '0');
+        f(v, s, ind + 1, num, used);
+        used[temp] = 0;
+        v.pop_back();
+    }
+    num = temp;
+
+    num *= 10;
+    num += (s[ind] - '0');
+    if(n >= ds[num] && num <= 50) f(v, s, ind + 1, num, used);
 }
 
 int main() {
-  cin >> s;
-  init();
-
+    cin >> s;
+    used.resize(55);
+    n = s.size();
+    init();
+    f(ans, s, 0, 0, used);
 }
