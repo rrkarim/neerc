8d6f9536ac59b1b2f2646071d8291dfc9dd36c7e
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-15 18:43:42 +0400

    filter.cpp added

--- neerc-semifinal-2014/filter.cpp
+++ neerc-semifinal-2014/filter.cpp
@@ -3,7 +3,7 @@
     Filter
 */
 #include <bits/stdc++.h>
-#define MAXN 1000005
+#define MAXN 100005
 #define INF 2e18
 #define _INF -20000000
 #define RANDOM_INIT()   srand(time(NULL))
@@ -12,6 +12,40 @@
 typedef long long ll;
 using namespace std;
 
+int m, f, a[105], q, h[1005][1005];
+int b[1005], dj[1005], n;
+string s;
 int main() {
+    cin >> m >> f;
+    for(int i = 0; i < f; ++i) cin >> a[i];
+    cin >> n;
+    for(int i = 0; i < n; ++i) {
+        cin >> s;
+        for(int j = 0; j < s.length(); ++j)
+            h[i][j] = (s[j] >= '0' && s[j] <= '9' ? s[j] - '0' : s[j] - 'a' + 10);
+    }
+    cin >> q;
+    ll temp;
+    int ans = 0;
+    for (int i = 0; i < q; i++) {
+        cin >> temp;
+        for (int j = 0; j < f; j++) dj[j] = (int)((temp * a[i]) % m);
+        for (int j = 0; j < n; j++) {
+            bool ald = 1;
+            for (int z = 0; z < f; z++) {
+                if ((h[j][dj[z] / 4] & (1 << (dj[z] % 4))) == 0)
+                    ald = false;
+            }
+            if (ald && !b[j]) {
+                b[j] = true;
+                ans++;
+            }
+        }
+    }
+    cout << ans;
+    for(int i = 0; i < n; ++i) if(b[i]) {
+        cout << " " << i;
+    }
+    cout << endl;
 
 }
