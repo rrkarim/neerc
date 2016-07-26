e26f122857aa6a5640a109d611933097be6f9e85
CoderINusE <rkerimov@std.qu.edu.az>
2016-07-26 13:46:38 +0400

    added spoj directory

--- spoj/PRIME1.cpp
+++ spoj/PRIME1.cpp
@@ -4,17 +4,18 @@
     Segmented sieve of Eratosthenes
 */
 #include <bits/stdc++.h>
-#define MAXN 10005
+#define MAXN 100005
 using namespace std;
 typedef pair<int,int> p;
 typedef long long ll;
 
 pair <int, int> a[MAXN + 2];
 vector <int> primes;
+int pr[MAXN + 2];
 
 void simple_eratosphen(ll limit) {
     int n = (int) sqrt(limit) + 1;
-    int d[n + 200];
+    int d[n + 3];
     memset(d, 0, sizeof(d));
     d[0] = d[1] = 1;
     for(int i = 2; i <= n; ++i) {
@@ -32,19 +33,18 @@ void simple_eratosphen(ll limit) {
 void erat(ll n) {
     simple_eratosphen(n);
 }
-
 void segment(ll l, ll r) {
-    int pr[r - l + 3000];
+    int len = r - l + 1;
     memset(pr, 0, sizeof(pr));
     for(int i = 0; i < primes.size(); ++i) {
-
         ll left = (l / primes[i]) * primes[i];
         if(left < l) left += primes[i];
-
         if(left == primes[i]) left += primes[i];
+
         if(left > r) break;
+
         while(left <= r) {
-            pr[left - l] = 1;
+            pr[(int)(left - l)] = 1;
             left += primes[i];
         }
     }
@@ -52,20 +52,19 @@ void segment(ll l, ll r) {
 
     for(int i = 0; i < r - l + 1; ++i) {
         if(pr[i] == 0) {
-            printf("%I64d\n", l + i);
+            printf("%lld\n", l + i);
         }
     }
     printf("\n");
-
 }
 
 int main() {
     int n; scanf("%d", &n);
     ll l, r;
     erat(1e9 + 1);
-
     for(int i = 0; i < n; ++i) {
-        scanf("%I64d%I64d", &l, &r);
+        scanf("%lld%lld", &l, &r);
+        assert(l <= r);
         segment(l, r);
     }
 }
