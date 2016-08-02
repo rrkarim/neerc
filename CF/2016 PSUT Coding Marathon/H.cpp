f4fdfa9076173487fb42a275e1c1b08abfb7b54b
CoderINusE <rkerimov@std.qu.edu.az>
2016-08-02 22:45:05 +0400

    added PSUT

--- CF/2016 PSUT Coding Marathon/H.cpp
+++ CF/2016 PSUT Coding Marathon/H.cpp
@@ -7,39 +7,35 @@ using namespace std;
 
 typedef long long ll;
 const ll mod = 1e9 + 7ll;
-int ks[10], a[MAXN + 2][10], used[10];
-int x[10] = {1, 5, 10, 20, 50}, n;
+ll ks[MAXN + 2];
+ll a[MAXN + 2][10], used[10];
+ll x[10] = {1, 5, 10, 20, 50}, n;
 int main() {
-    /**
-    //freopen("bonus.in", "r", stdin);
-    //freopen("bonus.out", "w", stdout);
-    */
-    cin >> n;
+    scanf("%d", &n);
     for(int i = 0; i < n; ++i) {
-        scanf("%d", ks + i);
-        for(int j = 0; j < 5; ++j) scanf("%d", &a[i][j]);
+        scanf("%I64d", &ks[i]);
+        for(int j = 0; j < 5; ++j) scanf("%I64d", &a[i][j]);
     }
     for(int i = 0; i < n; ++i) {
-        int sum = 0, k = ks[i];
+        ll sum = 0, k = ks[i];
         for(int j = 0; j < 5; ++j) {
             sum += a[i][j] * x[j];
         }
-        sum = sum - k;
-        for(int j = 0; j < 5; ++j) used[j] += a[i][j];
-
+        sum -= k;
         for(int j = 4; j >= 0; --j) {
-            if(sum >= x[j]) {
-                sum -= min(used[j], (sum / x[j])) * x[j];
-                used[j] -= min(used[j], (sum / x[j]));
+            if( sum >= x[j] ) {
+                ll d = sum / x[j];
+                d = min(d, used[j]);
+                sum -= d * x[j];
+                used[j] -= d;
             }
         }
-        if(sum) {
-            cout << "no" << endl;
+        if(sum != 0) {
+            printf("no\n");
             exit(0);
         }
-
-
+        for(int j = 0; j < 5; ++j) used[j] += a[i][j];
     }
-    cout << "yes" << endl;
 
+    printf("yes\n");
 }
