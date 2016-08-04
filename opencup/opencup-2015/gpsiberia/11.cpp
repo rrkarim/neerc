9fe130c3379f9e1615ac41e8de30facc271e182a
CoderINusE <rkerimov@std.qu.edu.az>
2016-08-04 13:52:44 +0400

    added PSUT

--- opencup/opencup-2015/gpsiberia/11.cpp
+++ opencup/opencup-2015/gpsiberia/11.cpp
@@ -1,7 +1,7 @@
 /**
 */
 #include <bits/stdc++.h>
-#define MAXN 211005
+#define MAXN 111005
 #define MINF -1000000000000
 using namespace std;
 
@@ -9,34 +9,39 @@ typedef long long ll;
 const ll mod = 1e9 + 7ll;
 
 int n, x, y, xt, yt, k, ks, m, t, as[MAXN + 2];
-vector <int> go[MAXN + 2];
-void dfs(int v) {
+vector <vector<int> > go;
+map <pair <int, int>, int > M;
+void dfs(const int& v) {
+    x += 1;
     ks += 1;
     as[v] = 1;
     for(int i = 0; i < go[v].size(); ++i) {
         int to = go[v][i];
-        if(!as[to]) dfs(to);
+        if(as[to] == 0) dfs(to);
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
+    //ios_base::sync_with_stdio(0);
     //freopen("input.txt", "r", stdin);
     //freopen("output.txt", "w", stdout);
-    cin >> n >> k >> m;
+    scanf("%d%d%d", &n, &k, &m);
+    go.resize(n + 2);
     for(int i = 0; i < k; ++i) {
-        cin >> t;
+        scanf("%d", &t);
         as[t] = 1;
     }
     for(int i = 0; i < m; ++i) {
-        cin >> xt >> yt;
-        go[yt].push_back(xt);
+        scanf("%d%d", &xt, &yt);
+        if(!M[{yt, xt}]) {
+            go[yt].push_back(xt);
+            M[{yt, xt}] = 1;
+        }
         if(as[yt] == 1) {
             ks = 0;
             dfs(yt);
             k += ks - 1;
         }
-        cout << k << endl;
+        printf("%d\n", k);
     }
-
 }
