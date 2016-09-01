1095c654e3c4ca159c4b5f84d4c900feb90c2162
CoderINusE <rkerimov@std.qu.edu.az>
2016-08-31 22:33:34 -0700

    31.08

--- archive/08.2016/CF369/A.cpp
+++ archive/08.2016/CF369/A.cpp
@@ -10,15 +10,12 @@ string s[1005];
 int main() {
     ios_base::sync_with_stdio(0);
     int n; cin >> n;
-    for(int i = 0; i < n; ++i) {
-        cin >> s[i];
-    }
+    for(int i = 0; i < n; ++i) cin >> s[i]; 
     int found = 0;
     for(int i = 0; i< n; ++i) {
         int f = 0;
         int c = 0;
         for(int j = 0; j < s[i].size(); ++j) {
-
             if(s[i][j] == '|') { f = 1, c = 0; continue; }
             if(f == 0) {
                 if(s[i][j] == 'O') c += 1;
