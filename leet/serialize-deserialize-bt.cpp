80f208b9abe35b78b4da03aaaad0e81b541673b8
CoderINusE <rkerimov@std.qu.edu.az>
2016-09-03 15:24:28 -0700

    03.09

--- leet/serialize-deserialize-bt.cpp
+++ leet/serialize-deserialize-bt.cpp
@@ -9,46 +9,39 @@
  */
 class Codec {
 public:
+    string st(int value) {
+        int c = 0;
+        if(value < 0) c = 1, value *= -1;;
+        string res; if(c == 1) res += '-';
+        while(value) {
+            res += (value % 10 + '0');
+            value /= 10;
+        }
+        reverse(res.begin() + c, res.end());
+        return res;
+    }
     string serialize(TreeNode* root) {
         string res = "[";
-        map<TreeNode*, TreeNode*> mp;
-        if(!root) return res;
-        TreeNode* p = root;
-        TreeNode* first = NULL;
-        TreeNode* last = NULL; 
-        while(p) {
-            int x = p->val;
-            string t;
-            while(x) {
-                t += ('0' + x % 10);
-                x /= 10;
-            }
-            reverse(t.begin(), t.end());
-            res += t; res += ",";
-            if(!first) {
-                if(p->left) first = p->left;
-                if(p->right) first = p->right;
-            }
-            if(p->left) {
-                if(last) {
-                    mp[last] = p->left;
-                }
-                last = p->left;
-            }
-            if(p->right) {
-                if(last) {
-                    mp[last] = p->right;
-                }
-                last = p->right;
-            } 
-            if(mp[p]) {
-                p = mp[p];
-            }
+        vector <string> ans;
+        if(root == NULL) return "[]";
+        queue <TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            TreeNode* t = q.front();
+            string x; 
+            if(t == NULL) x = "null", q.pop();
             else {
-                p = first;
-                last = first = NULL;
+                x = st(t->val);
+                q.push(t->left), q.push(t->right);
+                q.pop();
             }
+            ans.push_back(x);
         }
+        for(int i = ans.size() - 1; i >= 0; --i) {
+            if(ans[i] != "null") break;
+            ans.pop_back();
+        }
+        for(int i = 0; i < ans.size(); ++i) res += ans[i] + ",";
         res[res.length() - 1] = ']';
         return res;
     }
@@ -58,22 +51,25 @@ public:
         queue <TreeNode*> q;
         string x;
         int count = 0;
+        if(data == "[]") return root;
         for(int i = 1; i < data.size(); ++i) {
             if(data[i] == ',' || data[i] == ']') {
                 if(x == "null") {
+                    TreeNode* cu = q.front();
+                    if(count == 0) cu->left = NULL; 
+                    else cu->right = NULL;
                     count += 1;
-                    if(count == 2) {
-                        q.pop();
-                        count = 0;
-                    }
+                    if(count == 2) q.pop(), count = 0;
+                    x = "";
                     continue;
                 }
-                int d = 1, value = 0;
-                for(int i = x.length() - 1; i >= 0; --i) {
+                int d = 1, value = 0, cx = 0;
+                if(x[0] == '-') cx = 1;
+                for(int i = x.length() - 1; i >= cx; --i) {
                     value += d * (x[i] - '0');
                     d *= 10;
                 }
-                x = "";
+                if(cx) value *= -1; x = "";
                 if(root == NULL) {
                     root = new TreeNode(value);
                     q.push(root);
@@ -89,15 +85,10 @@ public:
                         q.push(cu->right);
                     }
                     count += 1;
-                    if(count == 2) {
-                        q.pop();
-                        count = 0;
-                    }
+                    if(count == 2) q.pop(), count = 0;
                 }
             }
-            else {
-                x += data[i];
-            }
+            else x += data[i];
         }
         return root;
     }
